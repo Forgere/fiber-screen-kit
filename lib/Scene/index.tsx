22dcbe8629a584ec9f589ced6335151151866dba
forgere <573596314@qq.com>
2024-03-30 01:35:40 +0000

    update: scene performance orbit envirionment

--- lib/Scene/index.tsx
+++ lib/Scene/index.tsx
@@ -1,5 +1,11 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { 
+  PerformanceMonitor, Environment, OrbitControls, Bvh,
+  type OrbitControlsProps,
+  type EnvironmentProps
+ } from '@react-three/drei'
+import { useRef, useState, Suspense } from 'react'
+import { Mesh } from 'three'
 
 type props = {
   position: [number, number, number]
@@ -7,12 +13,12 @@ type props = {
 
 function Box(props: props) {
   // This reference will give us direct access to the mesh
-  const meshRef = useRef<any>()
+  const meshRef = useRef<Mesh>(null)
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_, delta) => (meshRef.current.rotation.x += delta))
+  // useFrame((_, delta) => (meshRef.current?.rotation?.x += delta))
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
@@ -29,14 +35,35 @@ function Box(props: props) {
   )
 }
 
-export const Scene = () => {
+type TScene = {
+  envOptions?: EnvironmentProps
+  controlOptions?: OrbitControlsProps
+}
+
+export const Scene = (props: TScene = {}) => {
+  const { 
+    controlOptions = {},
+    envOptions = {
+      preset: 'city'
+    }
+  } = props  
+  const [dpr, setDpr] = useState(2)
+
   return (
-    <Canvas>
-      <ambientLight intensity={1} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <Suspense fallback={"loading"}>
+      {dpr}
+        <Canvas dpr={dpr}>
+          <PerformanceMonitor factor={1} onChange={({ factor }) => setDpr(Math.floor(0.5 + 1.5 * factor))} />
+          <Environment {...envOptions} />
+          <OrbitControls {...controlOptions} />
+            <ambientLight intensity={1} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+            <Bvh firstHitOnly>
+             <Box position={[-1.2, 0, 0]} />
+             <Box position={[1.2, 0, 0]} />
+            </Bvh>
+        </Canvas>   
+    </Suspense>
   )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import { Scene } from '../';
+import { Scene } from '../lib/main';
 
 function App() {
   const [count, setCount] = useState(0)
