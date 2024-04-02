4ac477a6dd207cc3f10195c1493c7d5554ef5137
zhangwei <forgere123zw@gmail.com>
2024-04-02 21:46:01 +0800

    Remove unused import in Context.tsx

--- lib/scene/Context.tsx
+++ lib/scene/Context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect } from 'react'
+import { createContext, useState, useContext } from 'react'
 import {PropsWithOptionalChildren} from '../utils'
 import * as THREE from 'three'
 
